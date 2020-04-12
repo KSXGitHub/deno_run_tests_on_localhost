ca52bd9de3403831118be3172f8c25c551e9c866
khai96_ <hvksmr1996@gmail.com>
2020-04-12 13:33:13 +0700

    Fix

--- main.ts
+++ main.ts
@@ -1,7 +1,7 @@
 import { start } from "./lib.ts";
 await start({
   addr: "0.0.0.0:4500",
-  target: "",
+  target: Deno.cwd(),
   cors: true,
   onServe(request) {
     console.log(`${request.method} ${request.url}`);
